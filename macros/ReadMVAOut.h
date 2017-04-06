38b5ffd3064e6d07677cc790d1f1c20e98944066
Jamal Rorie <jrorie@rice.edu>
2017-04-06 10:23:24 -0500

    Updated ReadMVAOut.C to take file names from config file. Made area for config files.

---
+++ macros/ReadMVAOut.h
@@ -0,0 +1,6 @@
+//****************************************************************************************************************
+// ReadMVAOut.C Configuration
+// -Change the value of "str_file_location" to the full path of the file you wish to access
+//**************************************************************************************************************** 
+
+string str_file_location = "/storage1/users/jtr6/EMTF_ML_training_results/PtRegression_v0_MLP.root"; 
---
+++ macros/configs/ReadMe.txt
@@ -0,0 +1,1 @@
+Store and edit your config files here with your initials appended to the end.  
