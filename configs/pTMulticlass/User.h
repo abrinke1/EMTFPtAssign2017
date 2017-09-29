3696bee68080d6e6d2cb8127d431fd0aaaaa87e7
Wei Shi <weishi@rice.edu>
2017-09-29 10:58:07 +0200

    add config for pT classification

---
+++ configs/pTMulticlass/User.h
@@ -0,0 +1,23 @@
+
+// *** Default user settings *** //
+TString OUT_DIR_NAME  = ".";  // Directory for output ROOT file
+TString OUT_FILE_NAME = "PtRegression_Apr_2017";  // Name base for output ROOT file
+TString EOS_DIR_NAME  = "root://eoscms.cern.ch//store/user/abrinke1/EMTF/Emulator/ntuples";  // Input directory in eos
+
+namespace PtRegression_Apr_2017_cfg {
+  
+  inline void ConfigureUser( const TString USER ) {
+    
+    std::cout << "\nConfiguring PtRegression_Apr_2017 code for user " << USER << std::endl;
+    
+    if (USER == "AWB") {
+      EOS_DIR_NAME = "root://eoscms.cern.ch//store/user/abrinke1/EMTF/Emulator/ntuples";  // Input directory in eos
+      OUT_DIR_NAME = "/afs/cern.ch/work/a/abrinke1/public/EMTF/PtAssign2017/files";
+      OUT_FILE_NAME = "PtRegression_Apr_2017_06_05_invPtTarg_invPtWgt";
+    }
+    if (USER == "JTR") {
+    }
+    
+  } // End function: inline void ConfigureUser()    
+  
+} // End namespace PtRegression_Apr_2017_cfg
