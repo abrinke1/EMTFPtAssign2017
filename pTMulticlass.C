f31b7b85e76a700eb520f3c7487531087f56a0c0
weishi10141993 <weishi@rice.edu>
2017-10-02 21:51:29 +0200

    Update pTMulticlass.C

--- pTMulticlass.C
+++ pTMulticlass.C
@@ -986,7 +986,7 @@ void pTMulticlass( TString myMethodList = "" ){
     if (Use["SVM"]) // Support Vector Machine
         factX->BookMethod( loadX,  TMVA::Types::kSVM, "SVM", "!H:!V:Gamma=0.25:Tol=0.001:VarTransform=Norm" );
     if (Use["MLP"]) // Multilayer Perceptron
-        factX->BookMethod( loadX,  TMVA::Types::kMLP, "MLP", "!H:!V:NeuronType=tanh:NCycles=1000:HiddenLayers=N,N+5,5:Sampling=0.7:SamplingEpoch=500:SamplingImportance=0.8:TestRate=5:EstimatorType=MSE:ConvergenceTests=10:VarTransform=Norm");
+        factX->BookMethod( loadX,  TMVA::Types::kMLP, "MLP", "!H:!V:NeuronType=tanh:NCycles=1000:HiddenLayers=N,N+5,5:Sampling=0.7:SamplingEpoch=500:SamplingImportance=0.8:TestRate=5:EstimatorType=MSE:ConvergenceTests=10:UseRegulator=True:VarTransform=Norm");
     if (Use["BDTG"]) // Gradient Boosted Decision Trees
         factX->BookMethod( loadX,  TMVA::Types::kBDT, "BDTG", "!H:!V:NTrees=400::BoostType=Grad:Shrinkage=0.1:nCuts=1000:MaxDepth=5:MinNodeSize=0.000001:RegressionLossFunctionBDTG=LeastSquares");
     if (Use["DNN"]) {
