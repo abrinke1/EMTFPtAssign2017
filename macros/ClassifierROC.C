9400091fe54d11035a15b0708eed78a98bc45e1d
weishi10141993 <weishi@rice.edu>
2017-11-10 16:02:17 +0100

    Create ClassifierROC.C

---
+++ macros/ClassifierROC.C
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TF1.h"
+#include "TAttFill.h"
+#include "TCanvas.h"
+#include <vector>
+#include "stdio.h"
+#include <stdlib.h>
+#include "math.h"
+#include "TMath.h"
+#include "TGraph.h"
+#include "TLegend.h"
+#include "TPaveStats.h"
+#include "TStyle.h"
+#include "THStack.h"
+#include "TFitResultPtr.h"
+
+
+//=***************************************************************************************
+//=Study the pT classifier performance and compare with BDT regression normally used at P5
+//=including ROC cureve, signal-to-background ratio
+//=
+//=Wei Shi @ Nov 11, 2017 CERN Geneva
+//=**************************************************************************************
+void ClassifierROC()
+{
+}
